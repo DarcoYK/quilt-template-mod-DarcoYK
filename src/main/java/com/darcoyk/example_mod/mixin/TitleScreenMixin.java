d4bffe275a10cb287066e945e4903f444ba65e46
DarcoYK <left.right.mommy@gmail.com>
2022-06-08 23:35:59 -0400

    updated name

--- src/main/java/com/darcoyk/example_mod/mixin/TitleScreenMixin.java
+++ src/main/java/com/darcoyk/example_mod/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package com.darcoyk.example_mod.mixin;
 
-import com.example.example_mod.ExampleMod;
+import com.darcoyk.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
